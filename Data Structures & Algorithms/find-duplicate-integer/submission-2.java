f2ed04d737d4c451b02484864d7227cf4b010afd
RallapalliGovardhanReddy2003 <144671902+RallapalliGovardhanReddy2003@users.noreply.github.com>
2026-07-09 17:17:01 +0530

    Add: find-duplicate-integer - submission-2

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-2.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        // Set<Integer> set = new HashSet<>();
+
+        // for (int num : nums) {
+        //     if (!set.add(num)) {
+        //         return num;
+        //     }
+        // }
+
+        // return -1;
+
+        int slow = nums[0];
+        int fast = nums[0];
+
+        // Phase 1: Find the meeting point
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+
+        // Phase 2: Find the entrance to the cycle
+        slow = nums[0];
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+
+        return slow;
+        
+    }
+}
